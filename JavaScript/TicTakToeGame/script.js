e8f8ab98ea66da7db805878d2f87609080746366
Chirag Tharad <chiragtharad01@gmail.com>
2026-05-26 11:33:04 +0530

    Improved JavaScript logic: winning conditions, looping strategies, redundant code.

--- JavaScript/TicTakToeGame/script.js
+++ JavaScript/TicTakToeGame/script.js
@@ -2,132 +2,66 @@ let count = 0;
 let gameArea = document.querySelector('.game-grid');
 let gridItems = document.querySelectorAll('.grid-item');
 let outcome = document.querySelector('.outcome');
+const checkGrid = () =>{
+    for(let i = 0;i<3;i++){
+        if(gridItems[i].innerText!="" && gridItems[i].innerText == gridItems[i+3].innerText && gridItems[i+3].innerText == gridItems[i+6].innerText){
+            return [i,i+3,i+6];
+        }
+        if(gridItems[3*i].innerText!="" && gridItems[(3*i)].innerText == gridItems[(3*i)+1].innerText && gridItems[(3*i)+1].innerText == gridItems[(3*i)+2].innerText){
+            return[(3*i),(3*i)+1,(3*i)+2];
+        }
+    }
+    if (gridItems[0].innerText!="" && gridItems[0].innerText == gridItems[4].innerText && gridItems[4].innerText == gridItems[8].innerText){
+        return [0,4,8];
+    }
+    if (gridItems[2].innerText!="" && gridItems[2].innerText == gridItems[4].innerText && gridItems[4].innerText ==  gridItems[6].innerText){
+        return [2,4,6];
+    }
+}
 gameArea.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('empty')){
+    if(e.target.classList.contains('grid-item') && e.target.innerText == ""){
         let img  = document.createElement('img');
-        
         if(count%2 === 0){
             img.setAttribute('src','./images/c.png')
-            e.target.classList.add('o');
-            e.target.classList.remove('empty');
+            e.target.innerText = 'o';
         }else{
             img.setAttribute('src','./images/x.png')
-            e.target.classList.add('x');
-            e.target.classList.remove('empty');
+            e.target.innerText = 'x';
         }
         e.target.appendChild(img);
         count++;
     }
-        if((gridItems[0].classList.contains('o') && gridItems[3].classList.contains('o') && gridItems[6].classList.contains('o')) || (gridItems[1].classList.contains('o') && gridItems[4].classList.contains('o') && gridItems[7].classList.contains('o')) || (gridItems[2].classList.contains('o') && gridItems[5].classList.contains('o') && gridItems[8].classList.contains('o')) || (gridItems[0].classList.contains('o') && gridItems[1].classList.contains('o') && gridItems[2].classList.contains('o')) || (gridItems[3].classList.contains('o') && gridItems[4].classList.contains('o') && gridItems[5].classList.contains('o')) || (gridItems[6].classList.contains('o') && gridItems[7].classList.contains('o') && gridItems[8].classList.contains('o')) || (gridItems[0].classList.contains('o') && gridItems[4].classList.contains('o') && gridItems[8].classList.contains('o')) || (gridItems[2].classList.contains('o') && gridItems[4].classList.contains('o') && gridItems[6].classList.contains('o'))){
-            if((gridItems[0].classList.contains('o') && gridItems[3].classList.contains('o') && gridItems[6].classList.contains('o'))){
-                gridItems[0].classList.add('color');
-                gridItems[3].classList.add('color');
-                gridItems[6].classList.add('color');
-            }
-            else if((gridItems[1].classList.contains('o') && gridItems[4].classList.contains('o') && gridItems[7].classList.contains('o'))){
-                gridItems[1].classList.add('color');
-                gridItems[4].classList.add('color');
-                gridItems[7].classList.add('color');
-            }else if((gridItems[2].classList.contains('o') && gridItems[5].classList.contains('o') && gridItems[8].classList.contains('o'))){
-                gridItems[2].classList.add('color');
-                gridItems[5].classList.add('color');
-                gridItems[8].classList.add('color');
-            }else if((gridItems[0].classList.contains('o') && gridItems[1].classList.contains('o') && gridItems[2].classList.contains('o'))){
-                gridItems[0].classList.add('color');
-                gridItems[1].classList.add('color');
-                gridItems[2].classList.add('color');
-            }else if((gridItems[3].classList.contains('o') && gridItems[4].classList.contains('o') && gridItems[5].classList.contains('o'))){
-                gridItems[3].classList.add('color');
-                gridItems[4].classList.add('color');
-                gridItems[5].classList.add('color');
-            }else if((gridItems[0].classList.contains('o') && gridItems[4].classList.contains('o') && gridItems[8].classList.contains('o'))){
-                gridItems[0].classList.add('color');
-                gridItems[4].classList.add('color');
-                gridItems[8].classList.add('color');
-            }
-            else if((gridItems[2].classList.contains('o') && gridItems[4].classList.contains('o') && gridItems[6].classList.contains('o'))){
-                gridItems[2].classList.add('color');
-                gridItems[4].classList.add('color');
-                gridItems[6].classList.add('color');
-            }
-            else{
-                gridItems[6].classList.add('color');
-                gridItems[7].classList.add('color');
-                gridItems[8].classList.add('color');
-            }
-            let h3 = document.createElement('h3');
-            h3.textContent = "o wins";
-            outcome.appendChild(h3);
-            let over = document.querySelector('.overlay');
-            over.classList.remove('hidden');
-        }
-        if((gridItems[0*3].classList.contains('x') && gridItems[1*3].classList.contains('x') && gridItems[2*3].classList.contains('x')) || (gridItems[1].classList.contains('x') && gridItems[4].classList.contains('x') && gridItems[7].classList.contains('x')) || (gridItems[2].classList.contains('x') && gridItems[5].classList.contains('x') && gridItems[8].classList.contains('x')) || (gridItems[0].classList.contains('x') && gridItems[1].classList.contains('x') && gridItems[2].classList.contains('x')) || (gridItems[3].classList.contains('x') && gridItems[4].classList.contains('x') && gridItems[5].classList.contains('x')) || (gridItems[6].classList.contains('x') && gridItems[7].classList.contains('x') && gridItems[8].classList.contains('x'))){
-            if((gridItems[0].classList.contains('x') && gridItems[3].classList.contains('x') && gridItems[6].classList.contains('x'))){
-                gridItems[0].classList.add('color');
-                gridItems[3].classList.add('color');
-                gridItems[6].classList.add('color');
-            }
-            else if((gridItems[1].classList.contains('x') && gridItems[4].classList.contains('x') && gridItems[7].classList.contains('x'))){
-                gridItems[1].classList.add('color');
-                gridItems[4].classList.add('color');
-                gridItems[7].classList.add('color');
-            }else if((gridItems[2].classList.contains('x') && gridItems[5].classList.contains('x') && gridItems[8].classList.contains('x'))){
-                gridItems[2].classList.add('color');
-                gridItems[5].classList.add('color');
-                gridItems[8].classList.add('color');
-            }else if((gridItems[0].classList.contains('x') && gridItems[1].classList.contains('x') && gridItems[2].classList.contains('x'))){
-                gridItems[0].classList.add('color');
-                gridItems[1].classList.add('color');
-                gridItems[2].classList.add('color');
-            }else if((gridItems[3].classList.contains('x') && gridItems[4].classList.contains('x') && gridItems[5].classList.contains('x'))){
-                gridItems[3].classList.add('color');
-                gridItems[4].classList.add('color');
-                gridItems[5].classList.add('color');
-            }else if((gridItems[0].classList.contains('x') && gridItems[4].classList.contains('x') && gridItems[8].classList.contains('x'))){
-                gridItems[0].classList.add('color');
-                gridItems[4].classList.add('color');
-                gridItems[8].classList.add('color');
-            }
-            else if((gridItems[2].classList.contains('x') && gridItems[4].classList.contains('x') && gridItems[6].classList.contains('x'))){
-                gridItems[2].classList.add('color');
-                gridItems[4].classList.add('color');
-                gridItems[6].classList.add('color');
-            }
-            else{
-                gridItems[6].classList.add('color');
-                gridItems[7].classList.add('color');
-                gridItems[8].classList.add('color');
-            }
-            let h3 = document.createElement('h3');
-            h3.textContent = "x wins";
-            outcome.appendChild(h3);
-            let over = document.querySelector('.overlay');
-            over.classList.remove('hidden');
-        }
+    let indexes = checkGrid();
+    if(indexes){
+        [0, 1, 2].forEach(x => gridItems[indexes[x]].classList.add('color'));
+        let h3 = document.createElement('h3');
+        h3.textContent = `${gridItems[indexes[0]].innerText} wins`;
+        let over = document.querySelector('.overlay');
+        over.classList.remove('hidden');
+        outcome.appendChild(h3);
+    }
+    else if(count == 9){
+        let h3 = document.createElement('h3');
+        h3.textContent = "Game Tied";
+        outcome.appendChild(h3);
+        let over = document.querySelector('.overlay');
+        over.classList.remove('hidden');
+        count = 0;
+    }
 })
 
 const resetGame = ()=>{
     let game_divs = document.querySelectorAll('.grid-item');
-    Array.from(game_divs).forEach((ele)=>{
-        if(ele.classList.contains('color')){
-            ele.classList.remove('color');
+    Array.from(game_divs).forEach((element)=>{
+        if(element.classList.contains('color')){
+            element.classList.remove('color');
         }
-        if(ele.classList.contains('o')){
-            ele.classList.remove('o')
-        }
-        if(ele.classList.contains('x')){
-            ele.classList.remove('x');
-        }
-        if(!ele.classList.contains('empty')){
-            ele.classList.add('empty');
-        }
-        
-        ele.replaceChildren()
+        element.innerText = ""
+        element.replaceChildren()
     })
     let over = document.querySelector('.overlay');
-    if(!over.classList.contains('hidden')){
-        over.classList.add('hidden');
-    }
+    over.classList.add('hidden')
     let outcome = document.querySelector('.outcome');
     outcome.replaceChildren();
+    count = 0
 }
